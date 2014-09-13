1bed40225cf186eb55a9698d5c1330e94d4d2684
Amadiro <jwringstad@gmail.com>
2014-09-13 18:17:29 +0200

    mouse-controlled blurriness

--- main.lua
+++ main.lua
@@ -3,12 +3,13 @@ _HEIGHT = love.graphics.getHeight()
 
 _SHADER = [[
 	extern Image kim;
+        extern float blurriness;
 
 	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
-		vec4 up = Texel(kim, texture_coords + vec2(0.01, 0.0));
-		vec4 down = Texel(kim, texture_coords + vec2(-0.01, 0.0));
-		vec4 left = Texel(kim, texture_coords + vec2(0.0, 0.01));
-		vec4 right = Texel(kim, texture_coords + vec2(0.0, -0.01));
+		vec4 up = Texel(kim, texture_coords + vec2(blurriness, 0.0));
+		vec4 down = Texel(kim, texture_coords + vec2(-blurriness, 0.0));
+		vec4 left = Texel(kim, texture_coords + vec2(0.0, blurriness));
+		vec4 right = Texel(kim, texture_coords + vec2(0.0, -blurriness));
 		vec4 middle = Texel(kim, texture_coords);
 
 		return (up + down + left + right + middle) / 4.0;
@@ -29,7 +30,11 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.setShader(shader)
+        local x, y = love.mouse.getPosition()
+        local blurriness = x / _WIDTH / 2.0
+	print(blurriness)
+	shader:send("blurriness", blurriness)
+        love.graphics.setShader(shader)
 	love.graphics.draw(osama, quad, _WIDTH / 2 - 25, _HEIGHT / 2 - 25)
 	love.graphics.setShader()
 end
