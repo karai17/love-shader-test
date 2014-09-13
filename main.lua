db439f1294d44622d3e6baf6bd9b4968e00548d3
Landon Manning <LManning17@gmail.com>
2014-09-13 12:29:03 -0300

    borkd ;_;

--- main.lua
+++ main.lua
@@ -2,36 +2,27 @@ _WIDTH = love.graphics.getWidth()
 _HEIGHT = love.graphics.getHeight()
 
 _SHADER = [[
-	extern vec4 green;
-	extern vec4 mouse;
+	extern Image kim;
 
 	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
-		return vec4(mouse.r, green.g, mouse.b, mouse.a);
+		return Texel(kim, texture_coords);
 	}
 ]]
 
 function love.load()
 	shader = love.graphics.newShader(_SHADER)
-	shader:send("green", { 0.0, 1.0, 0.0, 1.0 })
-
 	print(shader:getWarnings())
 
+	shader:send("kim", love.graphics.newImage("kim.png"))
 end
 
 function love.update(dt)
-	local x, y = love.mouse.getPosition()
-	x = x / _WIDTH
-	y = y / _HEIGHT
-	shader:send("mouse", { x, 0.0, y, 1.0 })
 
 end
 
 function love.draw()
-	love.graphics.setColor(255, 255, 0, 255)
-	love.graphics.rectangle("fill", 0, 0, _WIDTH, _HEIGHT)
-
 	love.graphics.setShader(shader)
-	love.graphics.rectangle("fill", 50, 50, _WIDTH-100, _HEIGHT-100)
+	love.graphics.rectangle("fill", 0, 0, _WIDTH, _HEIGHT)
 	love.graphics.setShader()
 end
 
