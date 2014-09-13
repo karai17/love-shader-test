33af70c4896a80ff087531dcbc8f6d2c2a28a36b
Landon Manning <LManning17@gmail.com>
2014-09-13 13:26:51 -0300

    Ooh~

--- main.lua
+++ main.lua
@@ -3,14 +3,14 @@ _HEIGHT = love.graphics.getHeight()
 
 _SHADER = [[
 	extern Image kim;
-        extern float blurriness;
+	extern vec2 blur;
 
 	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
-		vec4 up = Texel(kim, texture_coords + vec2(blurriness, 0.0));
-		vec4 down = Texel(kim, texture_coords + vec2(-blurriness, 0.0));
-		vec4 left = Texel(kim, texture_coords + vec2(0.0, blurriness));
-		vec4 right = Texel(kim, texture_coords + vec2(0.0, -blurriness));
-		vec4 middle = Texel(kim, texture_coords);
+		vec4 up		= Texel(kim, texture_coords + vec2( blur.x, 0.0));
+		vec4 down	= Texel(kim, texture_coords + vec2(-blur.x, 0.0));
+		vec4 left	= Texel(kim, texture_coords + vec2(0.0,  blur.y));
+		vec4 right	= Texel(kim, texture_coords + vec2(0.0, -blur.y));
+		vec4 middle	= Texel(kim, texture_coords);
 
 		return (up + down + left + right + middle) / 4.0;
 	}
@@ -30,11 +30,12 @@ function love.update(dt)
 end
 
 function love.draw()
-        local x, y = love.mouse.getPosition()
-        local blurriness = x / _WIDTH / 2.0
-	print(blurriness)
-	shader:send("blurriness", blurriness)
-        love.graphics.setShader(shader)
+	local x, y = love.mouse.getPosition()
+	local x = x / _WIDTH / 2.0
+	local y = y / _HEIGHT / 2.0
+
+	shader:send("blur", {x, y})
+	love.graphics.setShader(shader)
 	love.graphics.draw(osama, quad, _WIDTH / 2 - 25, _HEIGHT / 2 - 25)
 	love.graphics.setShader()
 end
