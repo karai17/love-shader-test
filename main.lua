a025124fd4f447d88fb02d1694506d8243bbc435
Landon Manning <LManning17@gmail.com>
2014-09-13 11:29:06 -0300

    :MAGIC:

--- main.lua
+++ main.lua
@@ -3,7 +3,8 @@ _HEIGHT = love.graphics.getHeight()
 
 _SHADER = [[
 	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
-		return vec4(1.0, 0.0, 0.0, 1.0);
+		return vec4(clamp(sin(screen_coords.x/100), 0, 1), clamp(cos(screen_coords.y/100), 0, 1), 0.0, 1.0);
+		//return vec4(1.0, 0.0, 0.0, 1.0);
 	}
 ]]
 
@@ -18,9 +19,10 @@ end
 
 function love.draw()
 	love.graphics.setColor(255, 255, 0, 255)
+	love.graphics.rectangle("fill", 0, 0, _WIDTH, _HEIGHT)
 
 	love.graphics.setShader(shader)
-	love.graphics.rectangle("fill", 0, 0, _WIDTH, _HEIGHT)
+	love.graphics.rectangle("fill", 50, 50, _WIDTH-100, _HEIGHT-100)
 	love.graphics.setShader()
 end
 
