a0c874972d8f94457e47ef888183b694ccaa5450
Landon Manning <LManning17@gmail.com>
2014-09-13 11:04:28 -0300

    words

--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
-function love.load()
+_WIDTH = love.graphics.getWidth()
+_HEIGHT = love.graphics.getHeight()
 
+_SHADER = [[
+	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
+		return vec4(1.0, 0.0, 0.0, 1.0);
+	}
+]]
+
+function love.load()
+	shader = love.graphics.newShader(_SHADER)
 end
 
 function love.update(dt)
@@ -7,5 +16,12 @@ function love.update(dt)
 end
 
 function love.draw()
+	love.graphics.setColor(255, 255, 0, 255)
+	love.graphics.rectangle("fill", 0, 0, _WIDTH, _HEIGHT)
+end
 
+function love.keypressed(key, isrepeat)
+	if key == "escape" then
+		love.event.quit()
+	end
 end
