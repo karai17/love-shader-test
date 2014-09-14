1e131f8875860663d149ad11e18be3d7d74d97d8
Landon Manning <LManning17@gmail.com>
2014-09-13 21:17:55 -0300

    Luaaaaaaaaaaaaaa

--- galaxy.lua
+++ galaxy.lua
@@ -3,40 +3,33 @@ local Galaxy = {}
 function Galaxy:init(shader, quality)
    self.time = 0
    local source = love.filesystem.read(shader)
-   if quality == "insane" then
-      source = source:gsub("$FRONT_LAYER_QUALITY", "300")
-      source = source:gsub("$BACK_LAYER_QUALITY", "150")
-      
-      source = source:gsub("$FRONT_LAYER_INTENSITY", "10000.0")
-      source = source:gsub("$BACK_LAYER_INTENSITY", "20000.0")
 
-      source = source:gsub("$DEFINE_EVOLVE", "#define EVOLVE")
-   elseif quality == "high" then
-      source = source:gsub("$FRONT_LAYER_QUALITY", "80")
-      source = source:gsub("$BACK_LAYER_QUALITY", "40")
-      
-      source = source:gsub("$FRONT_LAYER_INTENSITY", "5000.0")
-      source = source:gsub("$BACK_LAYER_INTENSITY", "8000.0")
+   self.quality = {}
+   self.quality.insane  = source:gsub("$FRONT_LAYER_QUALITY", "300")
+   self.quality.insane  = self.quality.insane:gsub("$BACK_LAYER_QUALITY", "150")
+   self.quality.insane  = self.quality.insane:gsub("$FRONT_LAYER_INTENSITY", "10000.0")
+   self.quality.insane  = self.quality.insane:gsub("$BACK_LAYER_INTENSITY", "20000.0")
+   self.quality.insane  = self.quality.insane:gsub("$DEFINE_EVOLVE", "#define EVOLVE")
 
-      source = source:gsub("$DEFINE_EVOLVE", "#define EVOLVE")
-   elseif quality == "medium" then
-      source = source:gsub("$FRONT_LAYER_QUALITY", "26")
-      source = source:gsub("$BACK_LAYER_QUALITY", "18")
+   self.quality.high    = source:gsub("$FRONT_LAYER_QUALITY", "80")
+   self.quality.high    = self.quality.high:gsub("$BACK_LAYER_QUALITY", "40")
+   self.quality.high    = self.quality.high:gsub("$FRONT_LAYER_INTENSITY", "5000.0")
+   self.quality.high    = self.quality.high:gsub("$BACK_LAYER_INTENSITY", "8000.0")
+   self.quality.high    = self.quality.high:gsub("$DEFINE_EVOLVE", "#define EVOLVE")
 
-      source = source:gsub("$FRONT_LAYER_INTENSITY", "10000.0")
-      source = source:gsub("$BACK_LAYER_INTENSITY", "15000.0")
+   self.quality.medium  = source:gsub("$FRONT_LAYER_QUALITY", "26")
+   self.quality.medium  = self.quality.medium:gsub("$BACK_LAYER_QUALITY", "18")
+   self.quality.medium  = self.quality.medium:gsub("$FRONT_LAYER_INTENSITY", "10000.0")
+   self.quality.medium  = self.quality.medium:gsub("$BACK_LAYER_INTENSITY", "15000.0")
+   self.quality.medium  = self.quality.medium:gsub("$DEFINE_EVOLVE", "#define EVOLVE")
 
-      source = source:gsub("$DEFINE_EVOLVE", "#define EVOLVE")
-   elseif quality == "low" then
-      source = source:gsub("$FRONT_LAYER_QUALITY", "20")
-      source = source:gsub("$BACK_LAYER_QUALITY", "10")
+   self.quality.low     = source:gsub("$FRONT_LAYER_QUALITY", "20")
+   self.quality.low     = self.quality.low:gsub("$BACK_LAYER_QUALITY", "10")
+   self.quality.low     = self.quality.low:gsub("$FRONT_LAYER_INTENSITY", "20000.0")
+   self.quality.low     = self.quality.low:gsub("$BACK_LAYER_INTENSITY", "30000.0")
+   self.quality.low     = self.quality.low:gsub("$DEFINE_EVOLVE", "")
 
-      source = source:gsub("$FRONT_LAYER_INTENSITY", "20000.0")
-      source = source:gsub("$BACK_LAYER_INTENSITY", "30000.0")
-
-      source = source:gsub("$DEFINE_EVOLVE", "")
-   end
-   self.fs = love.graphics.newShader(source)
+   self.fs = love.graphics.newShader(self.quality[quality])
    self.frequencies = {0.5, 0.5, 0.5, 0.5}
    print(self.fs:getWarnings())
 end
@@ -58,8 +51,9 @@ function Galaxy:update(dt)
 end
 
 function Galaxy:draw(w, h)
-   local x, y = love.mouse.getPosition()
-   self.fs:send("offset", {x/1000.0 - 0.5, y/1000.0 - 0.5})
+   -- half-assed attempt at motion. some sort of randomized pathing would be cool!
+   local x, y = math.sin(self.time / 16), math.cos(self.time / 12)
+   self.fs:send("offset", { x, y })
    self.fs:send("freqs", self.frequencies)
    self.fs:send("global_time", self.time)
    self.fs:send("global_time_sin", math.sin(self.time))
