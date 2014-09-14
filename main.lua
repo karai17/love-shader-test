4bf72d49e81aa31fe2873b4defd9827c93ecdec8
Landon Manning <LManning17@gmail.com>
2014-09-13 21:19:45 -0300

    Made it more lib-like

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ _HEIGHT	= love.graphics.getHeight()
 galaxy = require "galaxy"
 
 function love.load()
-	galaxy:init("galaxy.fsh", "high")
+	galaxy:init("galaxy/galaxy.fsh", "high")
 end
 
 function love.update(dt)
